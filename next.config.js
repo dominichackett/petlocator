58e9ca223c04cad7db94e0252dbff48d558e4f50
Dominic Leon Hackett <dominichackett@gmail.com>
2024-04-07 01:14:21 -0400

    Update next.config.js

--- next.config.js
+++ next.config.js
@@ -4,7 +4,15 @@ const nextConfig = {
   webpack: config => {
     config.externals.push('pino-pretty', 'lokijs', 'encoding');
     return config;
-  },typescript: {
+
+  },eslint: {
+    ignoreDuringBuilds: true,
+    rules: {
+      "react/no-unescaped-entities": "off",
+    }
+  },
+  
+  typescript: {
     // !! WARN !!
     // Dangerously allow production builds to successfully complete even if
     // your project has type errors.
